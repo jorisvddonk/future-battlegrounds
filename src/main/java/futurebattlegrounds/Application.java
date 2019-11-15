8550ea9d25e4e57338a21fd10f289cd4cb3c542c
Joris van de Donk <jorisvddonk@gmail.com>
2019-11-15 17:41:53 +0100

    emit OpenAPI spec

--- src/main/java/futurebattlegrounds/Application.java
+++ src/main/java/futurebattlegrounds/Application.java
@@ -1,7 +1,10 @@
 package futurebattlegrounds;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(info = @Info(title = "Future Battlegrounds", version = "0.0", description = "Future Battlegrounds API"))
 public class Application {
 
     public static void main(String[] args) {
